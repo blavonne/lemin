1832beda55a9922cd8d0ef8f23cf3eb7dd18a330
POLINA MAROVA <alkapolly@MacBook-Pro-POLINA.local>
2021-02-08 21:25:28 +0300

    ant manager fix

--- srcs/ant_management.c
+++ srcs/ant_management.c
@@ -12,31 +12,33 @@ t_ant			*create_ant(int	id, t_path *path, int step)
 	return (ant);
 }
 
-void	make_step(t_ant *ant, t_room **rooms, int end_id)
+void	make_step(t_ant *ant, t_room **rooms)
 {
 	int		step_index;
 	int		next_step;
 
-	step_index = ant->path->way[ant->cur_step];
-	next_step = ant->path->way[ant->cur_step + 1];
-	if (ant->path->way[ant->cur_step])
+	if (ant->cur_step < ant->path->len)
 	{
+		step_index = ant->path->way[ant->cur_step];
+		next_step = ant->path->way[ant->cur_step + 1];
 		if (rooms[step_index]->ant == NULL)
 		{
-			printf("%i %s ", ant->id, rooms[step_index]->name);
+			printf("L%d-%s ", ant->id, rooms[step_index]->name);
 			rooms[step_index]->ant = ant;
 			ant->cur_step++;
 		}
 		else
 		{
-			rooms[next_step]->ant = rooms[step_index]->ant;
-			printf("%d - %s ", rooms[next_step]->ant->id, rooms[next_step]->name);
-			rooms[next_step]->ant->cur_step++;
+			if (rooms[step_index]->is_end != 1)
+			{
+				rooms[next_step]->ant = rooms[step_index]->ant;
+				printf("L%d-%s ", rooms[next_step]->ant->id, rooms[next_step]->name);
+				rooms[next_step]->ant->cur_step++;
+			}
 			rooms[step_index]->ant = NULL;
-			make_step(ant, rooms, end_id);
+			make_step(ant, rooms);
 		}
 	}
-
 }
 
 void	ant_management(t_input *input, t_room **rooms, t_path **paths)
@@ -51,29 +53,34 @@ void	ant_management(t_input *input, t_room **rooms, t_path **paths)
 	finished = 0;
 	while (i < (size_t)input->ants)
 	{
-		ants[i] = create_ant(i + 1, paths[j], 0);
-		i++;
-		j++;
+		ants[i] = create_ant(i, paths[j++], 0);
 		if (j == input->path_arr->next && i < (size_t)input->ants)
 			j = 0;
+		i++;
 	}
 	i = 0;
 	j = 0;
-	while (ants[i]->path->way[ants[i]->cur_step] != input->end_id || finished != input->ants)
+	while (finished != input->ants)
 	{
-		if (ants[i]->path->way[ants[i]->cur_step] == input->end_id && ants[i]->id != -1)
+		if (i == (size_t)input->ants)
+			i = 0;
+		if (ants[i]->id != -1 && ants[i]->path->way[ants[i]->cur_step] == input->end_id)
 		{
 			finished++;
 			ants[i]->id = -1;
 			i++;
+			continue ;
+		}
+		if (ants[i]->id == -1)
+		{
+			i++;
+			continue;
 		}
 		if (ants[i]->path->way[ants[i]->cur_step] != input->end_id)
 		{
-			make_step(ants[i], rooms, input->end_id);
+			make_step(ants[i], rooms);
 			i++;
 		}
-		if (i == (size_t)input->ants)
-			i = 0;
 		j++;
 		if (j == input->path_arr->next)
 		{
